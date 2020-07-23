62adbd45679f605b296f21941bd97b47c10aaaff
younusraza909 <younusraza909@gmail.com>
2020-07-22 22:28:14 -0700

    Added Template ,Configure Static Files Added Css File navbr from Bootstrap

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.store, name="store"),
-    path("cart/", views.cart, name="store"),
-    path("checkout/", views.checkout, name="store"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
 ]
