2796a084d30cbca2c4be1a36f9ff55a6e7313822
younusraza909 <younusraza909@gmail.com>
2020-07-24 20:58:21 -0700

    Checkout page dynamical setup

--- store/views.py
+++ store/views.py
@@ -24,5 +24,13 @@ def cart(request):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        coustomer = request.user.coustomer
+        order, created = Order.objects.get_or_create(
+            coustomer=coustomer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_items': 0, "get_cart_total": 0}
+    context = {"items": items, "order": order}
     return render(request, "store/checkout.html", context)
