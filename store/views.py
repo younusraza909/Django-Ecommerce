9cb97867b1d77287cb8cf5a52bc56097acda9674
younusraza909 <younusraza909@gmail.com>
2020-07-24 20:48:45 -0700

    Adding Total of cart and quantiy of cart fucntionality added

--- store/views.py
+++ store/views.py
@@ -14,10 +14,12 @@ def cart(request):
         coustomer = request.user.coustomer
         order, created = Order.objects.get_or_create(
             coustomer=coustomer, complete=False)
+        # will get all order item attatch to that order
         items = order.orderitem_set.all()
     else:
         items = []
-    context = {"items": items}
+        order = {'get_cart_items': 0, "get_cart_total": 0}
+    context = {"items": items, "order": order}
     return render(request, "store/cart.html", context)
 
 
