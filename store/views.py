75035372e41ec872b766dca16d07287e4484bf8f
younusraza909 <younusraza909@gmail.com>
2020-07-27 20:39:54 -0700

    Making Views For Guest User Cart Product

--- store/views.py
+++ store/views.py
@@ -35,9 +35,23 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            # Getting Cookie In Python Backend
+            cart = json.loads(request.COOKIES["cart"])
+        except:
+            cart = {}
+
         items = []
         order = {'get_cart_items': 0, "get_cart_total": 0}
         cartItems = order["get_cart_items"]
+        for i in cart:
+            cartItems += cart[i]["quantity"]
+            # Making Total
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]['quantity'])
+            order['get_cart_items'] += cart[i]['quantity']
+            order['get_cart_total'] += total
+
     context = {"items": items, "order": order,
                "cartItems": cartItems, "shipping": False}
     return render(request, "store/cart.html", context)
