340175a1bfc2b8c94dc9f0d8b5154a355f2f0724
younusraza909 <younusraza909@gmail.com>
2020-07-28 01:00:58 -0700

    querying and displaying products for guest user

--- store/views.py
+++ store/views.py
@@ -45,12 +45,30 @@ def cart(request):
         order = {'get_cart_items': 0, "get_cart_total": 0}
         cartItems = order["get_cart_items"]
         for i in cart:
-            cartItems += cart[i]["quantity"]
-            # Making Total
-            product = Product.objects.get(id=i)
-            total = (product.price * cart[i]['quantity'])
-            order['get_cart_items'] += cart[i]['quantity']
-            order['get_cart_total'] += total
+            try:
+                cartItems += cart[i]["quantity"]
+                # Making Total
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]['quantity'])
+                order['get_cart_items'] += cart[i]['quantity']
+                order['get_cart_total'] += total
+
+                item = {
+                    'product': {
+                        "id": product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imageURL': product.imageURL
+                    },
+                    'quantity': cart[i]['quantity'],
+                    'get_total': total
+                }
+                items.append(item)
+
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass
 
     context = {"items": items, "order": order,
                "cartItems": cartItems, "shipping": False}
